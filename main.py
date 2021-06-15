a6a124bef7863b9d1655867cdac472214cae0516
PGAAPWdl <sasha.chakalov@gmail.com>
2021-06-15 22:39:44 +0300

    Upgrade

--- main.py
+++ main.py
@@ -14,28 +14,33 @@ def create_file(file_path, row_num):
             s.write('row ' + str(i) + " \n")
 
 
-def isPerfectSquare(x):
+def is_perfect_square(x):
+    """
+    Square number, a number which is the square of an integer
+
+    :param x:
+    :return:
+    """
     s = int(math.sqrt(x))
     return s * s == x
 
 
-def isFibonacci(n):
+def is_fibonacci(n):
     """
     n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both
     is a perfect square
     """
-    return isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)
+    return is_perfect_square(5 * n * n + 4) or is_perfect_square(5 * n * n - 4)
 
 
 def create_output(file_path, file_path_result):
     with open(file_path, 'r') as s:
-        for i in s:
-            y = i.split(" ")
-            if isFibonacci(int(y[1])) == True:
-                with open(file_path_result, 'a') as out:
-                    out.write(i)
-
-
+        with open(file_path_result, 'w') as out:
+            for i in s:
+                y = i.split(" ")
+                if is_fibonacci(int(y[1])):
+                    # reversing a string before adding in output.txt
+                    out.writelines(i[::-1])
 
 
 def main():
@@ -44,7 +49,6 @@ def main():
     row_nums = 10
     create_file(file_path, row_nums)
     create_output(file_path, file_path_result)
-
 
 
 if __name__ == "__main__":
